7ed05c422fdca686d1055c8b24a3e049ab3640bb
leogadil <leogadilvlog@gmail.com>
2022-10-02 13:18:32 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+	if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'on') {
+		$url = "https://";
+	} else {
+		$url = "http://";
+		$url.= $_SERVER['HTTP_HOST'];
+		$url.= $_SERVER['REQUEST_URI'];
+		$url;
+	}
+
+	$page = $url;
+	$sec = "10"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Portfolio</title>
+</head>
+<body>
+	hello there
+</body>
+</html>
